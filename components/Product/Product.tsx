7556ca7b15bca45fa5b075a7486d63e7b9d01deb
Lucas-Wilian <lucaswilianr.s@gmail.com>
2022-08-06 22:25:43 -0300

    Sexto commit: ProductCard

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import * as C from './styled';
+import Image from 'next/image';
+import ProductImg from '../Assets/clr.png';
 
 export default function Product() {
   return (
     <C.Container>
-      <C.ImgProduct />
+      <C.ValuePromotion>
+        <span>Desconto de 15%</span>
+      </C.ValuePromotion>
+
+      <C.ImgProduct>
+        <Image src={ProductImg} alt='testebimg' width={200} height={150} />
+      </C.ImgProduct>
+
       <C.NameProduct>
         <p>Smartphone Samsung Galaxy A13 128GB Lilas</p>
       </C.NameProduct>
--- components/Product/styled.ts
+++ components/Product/styled.ts
@@ -1,8 +1,39 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 200px;
+  height: 300px;
+  box-shadow: 4px 4px 4px ${({ theme }) => theme.colors.gray};
+  margin-bottom: 20px;
+  margin-top: 20px;
+`;
 
-export const ImgProduct = styled.div``;
+export const ValuePromotion = styled.div`
+  width: 100%;
+  height: 28px;
+  background: ${({ theme }) => theme.colors.green};
+  span {
+    text-align: center;
+    color: ${({ theme }) => theme.colors.white};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 14px;
+    font-weight: 600;
+    height: 100%;
+  }
+`;
+
+export const ImgProduct = styled.div`
+  margin: 10px 0px 10px 0px;
+  width: 100%;
+  height: auto;
+  max-height: 150px;
+  min-height: 140px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export const NameProduct = styled.div``;
 
